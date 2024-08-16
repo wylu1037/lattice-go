20cfa7e396493b44006c48cad0139adf0ba67c3e
wylu1037 <wylu1037@gmail.com>
2024-08-16 16:20:59 +0800

    chore: add func annotation

--- common/types/types.go
+++ common/types/types.go
@@ -22,11 +22,11 @@ func (n Number) MustToBigInt() *big.Int {
 
 // UploadFileResponse 文件上传到链上的返回结果
 //
-//   - CID 文件唯一标识
-//   - FilePath 文件存储地址
-//   - Message 返回信息
-//   - OccupiedStorageByte 文件占用的存储字节数，单位为byte
-//   - StorageAddress 需要冗余存储文件的节点地址
+//   - CID 文件唯一标识，示例：GPK3PveRaWoK6S2b53D3ZeJTm4nBvv2vSVjRStRLQcyX
+//   - FilePath 文件存储地址，示例：JG-DFS/tempFileDir/20240816/1723793768943848748_avatar.svg"
+//   - Message 返回信息，示例：success
+//   - OccupiedStorageByte 文件占用的存储字节数，单位为byte，示例：255686
+//   - StorageAddress 需要冗余存储文件的节点地址，示例：DFS_beforeSign||zltc_Z1pnS94bP4hQSYLs4aP4UwBP9pH8bEvhi;zltc_nBGgKoo1rzd4thjfauEN6ULj7jp1zXhxE;
 type UploadFileResponse struct {
 	CID                 string `json:"cid,omitempty"`
 	FilePath            string `json:"filePath,omitempty"`
